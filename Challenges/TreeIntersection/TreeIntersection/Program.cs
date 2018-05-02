98ee4f5eed46dcaa10b01855c83b24e8536c21a1
Joshua Taylor <taylor.joshua88@gmail.com>
2018-05-01 23:13:11 -0700

    Implemented algorithm and demo. Also implemented indexer for the HashTable class.

---
+++ Challenges/TreeIntersection/TreeIntersection/Program.cs
@@ -0,0 +1,128 @@
+﻿using BinaryTree;
+using HashTables;
+using System.Collections.Generic;
+using System;
+using StackAndQueue;
+
+namespace TreeIntersection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tree<int> treeA = new Tree<int>(150);
+            #region Populate treeA with nodes per this challenge's examples
+            {
+                BinaryTree.Node<int> left = treeA.Root.Left = new BinaryTree.Node<int>(100);
+                BinaryTree.Node<int> leftLeft = left.Left = new BinaryTree.Node<int>(75);
+                BinaryTree.Node<int> leftRight = left.Right = new BinaryTree.Node<int>(160);
+                BinaryTree.Node<int> leftRightLeft = leftRight.Left = new BinaryTree.Node<int>(125);
+                BinaryTree.Node<int> leftRightRight = leftRight.Right = new BinaryTree.Node<int>(175);
+
+                BinaryTree.Node<int> right = treeA.Root.Right = new BinaryTree.Node<int>(250);
+                BinaryTree.Node<int> rightLeft = right.Left = new BinaryTree.Node<int>(200);
+                BinaryTree.Node<int> rightRight = right.Right = new BinaryTree.Node<int>(350);
+                BinaryTree.Node<int> rightRightLeft = rightRight.Left = new BinaryTree.Node<int>(300);
+                BinaryTree.Node<int> rightRightRight = rightRight.Right = new BinaryTree.Node<int>(500);
+            }
+            #endregion
+
+            Console.WriteLine("Tree A in-order traversal values:");
+            Console.WriteLine($"[{string.Join(", ", treeA.InOrderTraversal(treeA.Root, new List<int>()))}]");
+
+            Tree<int> treeB = new Tree<int>(42);
+            #region Populate treeB with nodes per this challenge's examples
+            {
+                BinaryTree.Node<int> left = treeB.Root.Left = new BinaryTree.Node<int>(100);
+                BinaryTree.Node<int> leftLeft = left.Left = new BinaryTree.Node<int>(15);
+                BinaryTree.Node<int> leftRight = left.Right = new BinaryTree.Node<int>(160);
+                BinaryTree.Node<int> leftRightLeft = leftRight.Left = new BinaryTree.Node<int>(125);
+                BinaryTree.Node<int> leftRightRight = leftRight.Right = new BinaryTree.Node<int>(175);
+
+                BinaryTree.Node<int> right = treeB.Root.Right = new BinaryTree.Node<int>(600);
+                BinaryTree.Node<int> rightLeft = right.Left = new BinaryTree.Node<int>(200);
+                BinaryTree.Node<int> rightRight = right.Right = new BinaryTree.Node<int>(350);
+                BinaryTree.Node<int> rightRightLeft = rightRight.Left = new BinaryTree.Node<int>(4);
+                BinaryTree.Node<int> rightRightRight = rightRight.Right = new BinaryTree.Node<int>(500);
+            }
+            #endregion
+
+            Console.WriteLine();
+            Console.WriteLine("Tree B in-order traversal values:");
+            Console.WriteLine($"[{string.Join(", ", treeB.InOrderTraversal(treeB.Root, new List<int>()))}]");
+
+            Console.WriteLine();
+            Console.WriteLine("Set intersection of node values contained by trees A & B:");
+            Console.WriteLine($"[{string.Join(", ", TreeIntersection(treeA, treeB))}]");
+
+            Console.WriteLine();
+            Console.WriteLine("Please press any key to exit this demonstration...");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Returns the set of values contained by both treeA and treeB.
+        /// </summary>
+        /// <typeparam name="T">The type of values held by the binary trees and the returned set</typeparam>
+        /// <param name="treeA">The first binary tree</param>
+        /// <param name="treeB">The second binary tree</param>
+        /// <returns>Set of values contained by both trees as a collection</returns>
+        public static ICollection<T> TreeIntersection<T>(Tree<T> treeA, Tree<T> treeB)
+        {
+            HashTable<T, int> hashTable = new HashTable<T, int>();
+            MyQueue<BinaryTree.Node<T>> nodeQueue = new MyQueue<BinaryTree.Node<T>>(treeA.Root);
+            // Holds the intersection set of values to be returned to the caller
+            List<T> intersectionSet = new List<T>();
+
+            // Breadth-first traversal of treeA, adding each node's value to the hash table
+            while (nodeQueue.Length > 0)
+            {
+                BinaryTree.Node<T> currentNode = nodeQueue.Dequeue();
+
+                // HashTable's indexer will either add a new node or set the existing node's
+                // value by index (i.e. no duplicate nodes should result from this)
+                hashTable[currentNode.Value] = 0;
+
+                if (currentNode.Left != null)
+                {
+                    nodeQueue.Enqueue(currentNode.Left);
+                }
+                if (currentNode.Left != null)
+                {
+                    nodeQueue.Enqueue(currentNode.Right);
+                }
+            }
+
+            // Enqueue the root of treeB to start our traversal of the second tree
+            nodeQueue.Enqueue(treeB.Root);
+
+            // Breadth-first traversal of treeB. Will add values to intersectionSet
+            // that are contained by both trees only once to fit the return value
+            // being defined as a set.
+            while (nodeQueue.Length > 0)
+            {
+                BinaryTree.Node<T> currentNode = nodeQueue.Dequeue();
+
+                // Since the output is meant to be a set of values that appear in
+                // both trees, ensure that these values only appear once in the
+                // returned set by incrementing their hash tree values
+                if (hashTable.Contains(currentNode.Value).found &&
+                    hashTable[currentNode.Value]++ == 0)
+                {
+                    intersectionSet.Add(currentNode.Value);
+                }
+
+                if (currentNode.Left != null)
+                {
+                    nodeQueue.Enqueue(currentNode.Left);
+                }
+                if (currentNode.Left != null)
+                {
+                    nodeQueue.Enqueue(currentNode.Right);
+                }
+            }
+
+            return intersectionSet;
+        }
+    }
+}
--- Data_Structures/HashTables/HashTables/HashTable.cs
+++ Data_Structures/HashTables/HashTables/HashTable.cs
@@ -75,6 +75,34 @@ namespace HashTables
         }
 
         /// <summary>
+        /// Attempts to get a specific BucketNode object by key. Returns true and sets the
+        /// bucketNode out parameter to the node corresponding to the specified key. Returns
+        /// false if no nodes containing the specified key exist with the out parameter
+        /// set to null.
+        /// </summary>
+        /// <param name="key">The key to search for</param>
+        /// <param name="bucketNode">Out parameter set to the node corresponding to the
+        /// requested key if found; otherwise, this parameter will be null</param>
+        /// <returns>True if a node with the specified key exists; false otherwise</returns>
+        internal bool TryGetBucketNodeByKey(KeyT key, out BucketNode<KeyT, ValueT> bucketNode)
+        {
+            bucketNode = Buckets[GetHash(key)];
+
+            // Try to find the first bucket node that matches the provided key
+            while (bucketNode != null)
+            {
+                if (bucketNode.Key.Equals(key))
+                {
+                    break;
+                }
+
+                bucketNode = bucketNode.Next;
+            }
+
+            return bucketNode != null;
+        }
+
+        /// <summary>
         /// Tries to find the specified key within the hash table. If the key is
         /// found, then the found field of the returned value tuple is set to
         /// true and the value field is set to the value matching the key.
@@ -89,24 +117,13 @@ namespace HashTables
         /// default value of type ValueT.</returns>
         public (bool found, ValueT value) Contains(KeyT key)
         {
-            BucketNode<KeyT, ValueT> bucketNode = Buckets[GetHash(key)];
+            // Try to find a node with the specified key in the hash table
+            bool found = TryGetBucketNodeByKey(key, out BucketNode<KeyT, ValueT> bucketNode);
 
-            // Try to find the first bucket node that matches the provided key
-            while (bucketNode != null)
-            {
-                if (bucketNode.Key.Equals(key))
-                {
-                    break;
-                }
-
-                bucketNode = bucketNode.Next;
-            }
-
-            // Return a value tuple containing a boolean representing whether or not
-            // the specified key exists as well as that key's value if it exists (otherwise,
-            // the default value of ValueT is returned instead)
-            return (found: bucketNode != null,
-                    value: bucketNode != null ? bucketNode.Value : default(ValueT));
+            // Return a value tuple containing a bool based on whether a node matching the
+            // specified key exists as well a the value of that node or the default value for
+            // ValueT if the specified node does not exist.
+            return (found: found, value: bucketNode != null ? bucketNode.Value : default(ValueT));
         }
 
         /// <summary>
@@ -128,6 +145,35 @@ namespace HashTables
             // Push a new bucket node onto the bucket containing our new value
             int bucketIndex = GetHash(key);
             Buckets[bucketIndex] = new BucketNode<KeyT, ValueT>(key, value) { Next = Buckets[bucketIndex] };
+        }
+
+        /// <summary>
+        /// Provides a getter and setter for specific keys' values through an indexer.
+        /// Getter throws a KeyNotFoundException if the key does not exist. The setter
+        /// will either modify the existing, matching node's value or create a new one
+        /// containing the specified value if it does not yet exist.
+        /// </summary>
+        /// <param name="key">The key to either get or set the value for</param>
+        /// <returns>The value of the node corresponding to the specified key</returns>
+        /// <exception cref="KeyNotFoundException">A key was specified to the getter
+        /// that does not exist in the hash table.</exception>
+        public ValueT this[KeyT key] {
+            get {
+                (bool found, ValueT value) = Contains(key);
+                return found ? value : throw new KeyNotFoundException();
+            }
+            set {
+                // If the key already exists, change its corresponding node's value; otherwise
+                // add a new node matching the specified key with the specified value
+                if (TryGetBucketNodeByKey(key, out BucketNode<KeyT, ValueT> bucketNode))
+                {
+                    bucketNode.Value = value;
+                }
+                else
+                {
+                    Add(key, value);
+                }
+            }
         }
     }
 }
